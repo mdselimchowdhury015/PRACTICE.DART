9318f59e8cd7d750a7a2acb34324264ac0dbad5a
mdselimchowdhury015 <mdselimchowdhury01567@gmail.com>
2023-08-28 13:21:01 +0600

    first commit

---
+++ bin/practice.dart
@@ -0,0 +1,20 @@
+void main (){
+
+ var city=['Dhaka','Rangpur','Nilphamari','Thakurgoan'];
+
+print(city);
+
+
+city.add("Dinajpur");
+
+print(city);
+
+
+city.add("Khulna");
+
+
+  print (city);
+
+}
+
+
